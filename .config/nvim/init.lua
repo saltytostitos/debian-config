dd67c66954ec82f8e11ee616af3be4d4bffd9be9
Sean LeCheminant <sean@pop-os.lan1>
2024-12-30 19:02:27 -0600

    Installed LazyVim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,2 @@
-require("plugins")
-require("whichkey")
-require("telescope")
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-require("nvim-tree").setup()
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
