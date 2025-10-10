868ee05fc599f110299c916e4aa239f8df970425
Sean LeCheminant <32964349+saltytostitos@users.noreply.github.com>
2025-10-10 10:28:16 -0500

    Disabled Snacks Explorer, Enabled Yazi

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+    keys = {
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+      { "<leader>E", false },
+      { "<leader>e", false },
+    },
+  },
+}
